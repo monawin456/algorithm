94ef0f5c342a45446e5e733335306a112807a4cb
황정태 <monawin456@gmail.com>
2016-11-23 02:12:38 +0900

    v1.0

--- GraphColoring(Backtracking)/Source.cpp
+++ GraphColoring(Backtracking)/Source.cpp
@@ -1,16 +1,20 @@
 #include<iostream>
 
-bool m_coloring(int i);
+void m_coloring(int i);
 bool promising(int i);
 
-int n;
-int m;
-int **W;
-int *vcolor;
+int n; //number of vertices
+int m; //number of colors
+int **W; //graph
+int *vcolor; //result
+
+int i; //index
+bool coloring;
+int num; //number of all cases
 
 int main() {
+	//set condition
 	n = 5;
-	m = 4;
 
 	W = new int*[n + 1];
 	for (int i = 0; i <= n; i++) {
@@ -21,21 +25,33 @@ int main() {
 			W[i][j] = 0;
 		}
 	}
-
-	vcolor = new int[n + 1];
-	for (int i = 0; i <= n; i++) {
-		vcolor[i] = 0;
-	}
-
 	W[1][2] = 1;	W[1][3] = 1;	W[1][4] = 1;
 	W[2][1] = 1;	W[2][3] = 1;	W[2][5] = 1;
 	W[3][1] = 1;	W[3][2] = 1;	W[3][4] = 1;	W[3][5] = 1;
 	W[4][1] = 1;	W[4][3] = 1;	W[4][5] = 1;
 	W[5][2] = 1;	W[5][3] = 1;	W[5][4] = 1;
 
-	int i = 1;
-	m_coloring(i);
+	vcolor = new int[n + 1];
+	for (int i = 0; i <= n; i++) {
+		vcolor[i] = 0;
+	}
 
+	//start coloring
+	m = 1;
+	i = 0;
+	coloring = false;
+	num = 0;
+
+	while(!coloring){
+		m_coloring(i);
+		m++;
+		i = 0;
+	}
+
+	std::cout << "Least number of colors: " << m << std::endl;
+	std::cout << "Number of all cases: " << num << std::endl;
+
+	//delete condition
 	for (int i = 0; i <= n; i++) {
 		delete[]W[i];
 	}
@@ -43,18 +59,20 @@ int main() {
 
 	delete[]vcolor;
 
+	//end
 	return 0;
 }
 
-bool m_coloring(int i) {
+void m_coloring(int i) {
 	int color;
 	if (promising(i)) {
 		if (i == n) {
-			// cout << vcolor[1] through vcolor[n];
 			for (int i = 1; i <= n; i++) {
 				std::cout << "v" << i << "=" << vcolor[i] << " ";
 			}
 			std::cout << std::endl;
+			coloring = true;
+			num++;
 		}
 		else {
 			for (color = 1; color <= m; color++) {
@@ -63,23 +81,18 @@ bool m_coloring(int i) {
 			}
 		}
 	}
-	else {
-		return false;
-
-	}
-	return true;
 }
 
 bool promising(int i) {
 	int j;
-	bool mSwitch;
-	mSwitch = true;
+	bool pSwitch;
+	pSwitch = true;
 	j = 1;
-	while (j<i && mSwitch) {
+	while (j<i && pSwitch) {
 		if (W[i][j] && (vcolor[i] == vcolor[j])) {
-			mSwitch = false;
+			pSwitch = false;
 		}
 		j++;
 	}
-	return mSwitch;
+	return pSwitch;
 }
